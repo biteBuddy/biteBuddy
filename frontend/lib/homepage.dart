c6249a71c065ae00891446d114ed44fea0871f3f
1shan-pixel <ispanta03@gmail.com>
2023-04-09 05:37:19 +0545

    Commit -2 Needs some frontend work, backend not initialized

---
+++ frontend/lib/homepage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'My Home Page',
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+      ),
+    );
+  }
+}
