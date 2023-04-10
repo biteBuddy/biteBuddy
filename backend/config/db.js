4988d7ab1b757783ca3fce88e0c5ee652b8872d1
PriyanshuSharma1 <psneb420@gmail.com>
2023-04-10 14:25:59 +0545

    MongoDb connected

---
+++ backend/config/db.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const connection = mongoose.createConnection("mongodb://127.0.0.1:27017").on("open",()=>{
+    console.log("MongoDb connected");
+}).on("error",()=>{
+    console.log("MongoDb connection error!!")
+});
+
+module.exports=connection;
--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const app= require("./app");
+const db= require("./config/db")
 
 const port=3000;
 app.get("/",(req,res)=>{
