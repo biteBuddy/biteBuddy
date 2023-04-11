c23fbe8551920b6e8eb28177046000a31403387d
PriyanshuSharma1 <psneb420@gmail.com>
2023-04-11 14:05:50 +0545

    User registration api testing completed

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,11 @@
 //makes use of express js to make use of routes and other things
 const express= require("express"); //importing the package
-const app = express(); // making use of variable app to import the module (like creating instance)
+const body_parser=require("body-parser");
+const userRouter = require("./routers/user.router");
+
+const app = express();// making use of variable app to import the module (like creating instance)
+app.use(body_parser.json());
+
+app.use("/",userRouter);
+
 module.exports=app; //exporting the app
--- backend/services/user.services.js
+++ backend/services/user.services.js
@@ -1,5 +1,13 @@
+const UserModel = require("../model/user.model")
 class UserServices{
     static async registerUser(email,password){
-        
+        try{
+            const createUser= new UserModel({email,password}); //store data
+            return await createUser.save(); //stores in database
+
+        }catch(err){
+            throw err;
+        }
     }
 }
+module.exports= UserServices;
